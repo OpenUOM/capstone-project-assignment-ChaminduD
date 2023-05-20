45341513c4cb76e83d3cf5ce5e8261c850df0e8a
ChaminduD <chamindudulanjana10@gmail.com>
2023-05-20 16:01:30 +0530

    database function changes

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -65,9 +65,9 @@ export class TeacherTableComponent implements OnInit {
     if (value.length <= 0) {
       this.getTeacherData();
     } else {
-      let b = this.teacherData.filter((teacher) => {
-        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
-          foundItems.push(teacher)
+      foundItems = this.teacherData.filter((teacher) => {
+        if (teacher[0].name.toLowerCase().includes(value.toLowerCase())) {
+          return true;
         }
       });
       this.teacherData = foundItems;
